d6cb8af1addcd347a93f26b6d5b0bee71faa2fbf
Pa Yeng Thao <payengt24@gmail.com>
2019-02-15 23:01:02 -0600

    updated BrowserRouter to HashRouter for deployment to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Header from "./components/layout/Header";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "./Context";

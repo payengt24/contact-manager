3d7762d031a488de9f117b1c0664ca7fec3c4cf6
Pa Yeng Thao <payengt24@gmail.com>
2019-02-15 23:04:42 -0600

    Updates

---
+++ precache-manifest.ee5365361c15971ce42b6f323fc0920f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "d61477897ebf98887941",
+    "url": "/contact-manager/static/js/main.d6147789.chunk.js"
+  },
+  {
+    "revision": "d242daf19cd6de44bab8",
+    "url": "/contact-manager/static/js/2.d242daf1.chunk.js"
+  },
+  {
+    "revision": "d61477897ebf98887941",
+    "url": "/contact-manager/static/css/main.e6257bfd.chunk.css"
+  },
+  {
+    "revision": "d242daf19cd6de44bab8",
+    "url": "/contact-manager/static/css/2.f6a9da8c.chunk.css"
+  },
+  {
+    "revision": "108e535145362f9263bc63eac8588e44",
+    "url": "/contact-manager/index.html"
+  }
+];
